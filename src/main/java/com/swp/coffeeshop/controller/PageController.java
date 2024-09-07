6dea7199ee7c815319bcd0ff067c3cf4391ea8f5
sondmfpt <sondmhe186291@fpt.edu.vn>
2024-09-07 17:11:58 +0700

    login page interface

---
+++ src/main/java/com/swp/coffeeshop/controller/PageController.java
@@ -0,0 +1,13 @@
+package com.swp.coffeeshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String showHome() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/swp/coffeeshop/security/CaffeShopSecurity.java
@@ -0,0 +1,54 @@
+package com.swp.coffeeshop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class CaffeShopSecurity {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails son = User.builder()
+                .username("son")
+                .password("{noop}123")
+                .roles("CUSTOMER", "ADMIN")
+                .build();
+
+        UserDetails linh = User.builder()
+                .username("linh")
+                .password("{noop}123")
+                .roles("CUSTOMER")
+                .build();
+
+        return new InMemoryUserDetailsManager(son, linh);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/").hasAnyRole("ADMIN", "CUSTOMER")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(configurer ->
+                        configurer.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer
+                                .accessDeniedPage("/access-denied")
+                );
+        return http.build();
+    }
+}
