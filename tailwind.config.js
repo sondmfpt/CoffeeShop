1ec855c56b73b5e84a1fc5df4d68ec41558a79ea
sondmfpt <sondmhe186291@fpt.edu.vn>
2024-09-08 01:07:24 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        'coffee': {
-          200: '#eee1c4',
-          500: '#dcb386',
-          700: '#9e826c' //hover
-        }
-      },
-      fontFamily: {
-        'serif': "'Abril Fatface', serif"
-      }
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'coffee': {
+                    200: '#eee1c4',
+                    500: '#dcb386',
+                    700: '#9e826c' //hover
+                }
+            },
+            fontFamily: {
+                'serif': "'Abril Fatface', serif"
+            },
+            backgroundImage: {
+                'login-bg': "url('../static/img/pexels-jonathanborba-2878712.jpg')",
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
